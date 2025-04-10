55556e0a2a0cb3df0d4f0d81a8e07a6fbdbab47c
JuanC <juancabrujav@gmail.com>
2025-04-10 19:43:26 +0200

    primera implementacion muy pobre

--- src/Components/Calculadora.js
+++ src/Components/Calculadora.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { StatsContext } from '../Context/StatsContext';
 import CalculadoraDaños from './CalculadoraDaños';
 import CalculadoraDeStats from './CalculadoraDeStats';
-import { useContext } from 'react';
 import Estadisticas from './Estadisticas';
-
+import BuscadorDeTecnicas from './BuscadorDeTecnicas'; // Importar el nuevo componente
 
 const Calculadora = () => {
   const {
@@ -24,23 +23,35 @@ const Calculadora = () => {
   } = useContext(StatsContext);
 
   return (
-    
     <div className="flex flex-col min-h-screen">
       <main className="flex-grow">
-    <div className="flex flex-wrap lg:flex-nowrap justify-center items-start gap-6 p-6 bg-papiro bg-cover bg-center bg-blend-multiply bg-narutoLight text-narutoDark max-w-screen-xl mx-auto">
-     <CalculadoraDeStats />
-      {/* Columna derecha */}
-      <div className="flex flex-col gap-6 w-full lg:w-1/3">
-        <Estadisticas />
-        <CalculadoraDaños />
-      </div>
+        <div className="flex flex-wrap lg:flex-nowrap justify-center items-start gap-6 p-6 bg-papiro bg-cover bg-center bg-blend-multiply bg-narutoLight text-narutoDark max-w-screen-xl mx-auto">
+          {/* Calculadora de Stats */}
+          <CalculadoraDeStats />
+          {/* Columna derecha */}
+          <div className="flex flex-col gap-6 w-full lg:w-1/3">
+            <Estadisticas />
+            <CalculadoraDaños />
+          </div>
+        </div>
+        {/* Buscador de Técnicas: Ahora está dentro del contenedor con fondo */}
+        <div className="flex flex-col gap-6 p-6 bg-papiro bg-cover bg-center bg-blend-multiply bg-narutoLight text-narutoDark max-w-screen-xl mx-auto">
+          <BuscadorDeTecnicas />
+        </div>
+        <footer className="text-center text-sm text-gray-600 font-semibold mt-6">
+          Work in progress by{' '}
+          <a
+            href="https://cabruja.dev"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-narutoOrange hover:underline"
+          >
+            cabruja.dev
+          </a>{' '}
+          ®. 2025 ♡
+        </footer>
+      </main>
     </div>
-
-  <footer className="text-center text-sm text-gray-600 font-semibold mt-6">
-    Work in progress by <a href="https://cabruja.dev" target="_blank" rel="noopener noreferrer" className="text-narutoOrange hover:underline">cabruja.dev</a> ®. 2025 ♡
-  </footer>
-  </main>
-</div>
   );
 };
 
